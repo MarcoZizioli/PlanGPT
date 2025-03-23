b606274781e1f51f44b1dd061f682ca8cd0ecd2c
MarcoZizioli <133860854+MarcoZizioli@users.noreply.github.com>
2025-03-23 14:25:07 +0100

    Add files via upload

---
+++ driverlog/driverlog-code/greedy/code-for-generation/generation_launcher_driverlog.py
@@ -0,0 +1,45 @@
+import subprocess
+import json
+import os
+import re
+
+
+def main():
+    domains = ["driverlog"]
+
+    '''
+    "top_p": 0.95,
+    "do_sample": True,
+    "num_return_beams": 10,
+    '''
+    for domain in domains:
+        for mode in ["greedy"]:
+            print(f"### Generazione con {domain} e {mode}")
+            path = f"config.json"
+            params = {
+                "domain": domain,
+                "model_dir": f"/root/mzizioli/planGPT-OGR/models/{domain}/", 
+                "dataset_dir": f"/root/mzizioli/planGPT-OGR/{domain}-dataset-tesi/",
+                "num_beams": 1,
+                "num_return_beams": 1,
+                "split_dataset": "test",
+                "output_dir": f"/root/mzizioli/planGPT-OGR/{domain}-generations-tesi/greedy/",
+            }
+            with open(path, "w") as file:
+                tmp = json.dumps(params, indent=4)
+                file.write(tmp)
+            with open(path, "r") as file:
+                # Carica il contenuto del file JSON in una variabile
+                data = json.load(file)
+            print("### Generazione con " + path)
+            print(json.dumps(data, indent=4))            
+            
+            subprocess.call(
+                [
+                    "python3",
+                    "planGPT-OGR/scripts/plans_generator_Action_driverlog.py",
+                    path,
+                ]
+            )
+
+main()
